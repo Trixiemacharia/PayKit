1d7034e17738d0a03aa5ce8ac06a408248f1df8f
TrixieMacharia <machariastacy034@gmail.com>
2026-07-07 12:45:20 +0300

    Update Project

--- paykit/apps/subscriptions/admin.py
+++ paykit/apps/subscriptions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Plan, Subscription
 
-# Register your models here.
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ["name", "price_kes", "billing_cycle", "is_active"]
+    list_editable = ["price_kes", "is_active"]
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ["user", "plan", "status", "start_date", "end_date"]
